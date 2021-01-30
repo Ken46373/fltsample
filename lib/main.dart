cc5cb5b85505ca15ddd78709ec4d620307485e25
Ken Minami <kenminami.dev@gmail.com>
2021-01-30 23:46:59 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,65 @@
-/* 1-4. ListView */
+/* 3-1. ボタン */
 
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
-void main() => runApp(MyApp()); // 引数のWidgetが画面いっぱいに表示される
+void main() => runApp(MyApp());
 
-// 状態を管理するクラスは、Stateクラスを継承
-class RandomWordsState extends State<RandomWords> {
-  // _を付けるとライブラリ外からアクセスできなくなる
-  // ListViewのWidgetを作るメソッド
-  Widget _buildSuggestions() {
-    final _wordPairs = <WordPair>[];  // 単語のペアを格納するリスト
-    return ListView.builder(
-        itemBuilder: (context, i) { // itemBuilderで一行ごとに処理が呼ばれる
-          if (i.isOdd) return Divider();  // 奇数行には水平線を表示
-
-          final index = i ~/ 2;  // ~/は結果が整数の割り算
-          if (index >= _wordPairs.length) {  // 行数がリストの要素数を越えれば
-            _wordPairs.addAll(generateWordPairs().take(10));  // 単語のペアを10個追加
-          }
-          return _buildRow(_wordPairs[index]);
-        }
-    );
-  }
-
-  // 単語のペアから、形式を整えた行のWidgetを作るメソッド
-  Widget _buildRow(WordPair pair) {
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-      ),
-    );
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Live!人工知能"),
+    return MaterialApp(
+      title: "My Simple App",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Live!人工知能"),
+        ),
+        body: Center(
+          child: MyForm(),
+        ),
       ),
-      body: _buildSuggestions(),
     );
   }
 }
 
-// StatefulなWidgetのクラスは、StatefulWidgetを継承
-class RandomWords extends StatefulWidget {
+class MyForm extends StatefulWidget {
   @override
-  RandomWordsState createState() => RandomWordsState();
+  _MyFormState createState() => _MyFormState();
 }
 
-// 最初に表示するWidgetのクラス
-class MyApp extends StatelessWidget {  // StatelessWidgetを継承
-  @override
-  Widget build(BuildContext context) {  //buildメソッドでUIを作成
-    return MaterialApp(  // マテリアルデザインのアプリ
-        title: "My Simple App",  // アプリのタイトル
-        home: RandomWords()
+class _MyFormState extends State<MyForm> {
+  int _count = 0;
+
+  void _handlePressed() {
+    setState(() {  // 状態を保持する変数を変更する処理は、setState内に記述する
+      _count++;
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "$_count",
+              style: TextStyle(
+                color:Colors.blueAccent,
+                fontSize: 30.0,
+              ),
+            ),
+            FlatButton(  // 一番シンプルなボタン
+              onPressed: _handlePressed,
+              color: Colors.blue,
+              child: Text(
+                "いいね!",
+                style: TextStyle(
+                    color:Colors.white,
+                    fontSize: 20.0
+                ),
+              ),
+            )
+          ],
+        )
     );
   }
 }
